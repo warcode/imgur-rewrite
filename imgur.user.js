bcc047c539ff6033a97815a4849d33ba5b45b5cf
warcode <warcode@deny.io>
2014-05-24 18:43:56 +0200

    www fix

--- imgur.user.js
+++ imgur.user.js
@@ -1,54 +1,59 @@
 // ==UserScript==
-// @version 1.0.8
+// @version 1.0.9
 // @name           Imgur rewrite 
 // @namespace      http://www.reddit.com/*
 // @description    Makes imgur links into the direct link
 // @include        http://www.reddit.com/*
 // ==/UserScript==
-
-
-var entries = document.getElementsByTagName("a");
-
-for(var i = 0; i != entries.length; i++)
+var entries = document.getElementsByTagName('a');
+for (var i = 0; i != entries.length; i++)
 {
-	var link = entries[i];
-
-	//ignore album links
-	if((link.href.indexOf("imgur.com/a/") == -1) && (link.href.indexOf("imgur.com/gallery/") == -1))
-	{
-		if(link.href.indexOf("imgur.com") != -1)
-		{
-
-			if(link.href.split(".").length < 3)
-			{
-				link.title = "Original at: " + link.href;
-				link.href += ".png";
-				link.href = wtf_replace(link.href, "imgur.com", "i.imgur.com");
-			}
-            else if(link.href.indexOf("m.imgur.com") != -1)
-            {
-                link.title = "Original at: " + link.href;
-				link.href = wtf_replace(link.href, "m.imgur.com", "i.imgur.com");
-                link.href += ".png";
-            }
-			else if(link.href.indexOf("i.imgur.com") == -1)
-			{
-				link.title = "Original at: " + link.href;
-				link.href = wtf_replace(link.href, "imgur.com", "i.imgur.com");
-			}
-			else if(link.href.indexOf(",") != -1)
-			{
-				link.title = "Original at: " + link.href;
-				link.href = link.href.split(",").length;
-				link.href += ".png";
-				link.href = wtf_replace(link.href, "imgur.com", "i.imgur.com");
-			}
-		}
-	}
-
+  var link = entries[i];
+  //ignore album links
+  if ((link.href.indexOf('imgur.com/a/') == - 1) && (link.href.indexOf('imgur.com/gallery/') == - 1))
+  {
+    if (link.href.indexOf('imgur.com') != - 1)
+    {
+      if (link.href.split('.') .length < 3)
+      {
+        link.title = 'Original at: ' + link.href;
+        link.href += '.png';
+        link.href = wtf_replace(link.href, 'imgur.com', 'i.imgur.com');
+        if (link.href.indexOf('www.') != - 1) {
+          link.href = wtf_replace(link.href, 'www.', '');
+        }
+      } 
+      else if (link.href.indexOf('m.imgur.com') != - 1)
+      {
+        link.title = 'Original at: ' + link.href;
+        link.href = wtf_replace(link.href, 'm.imgur.com', 'i.imgur.com');
+        link.href += '.png';
+        if (link.href.indexOf('www.') != - 1) {
+          link.href = wtf_replace(link.href, 'www.', '');
+        }
+      } 
+      else if (link.href.indexOf('i.imgur.com') == - 1)
+      {
+        link.title = 'Original at: ' + link.href;
+        link.href = wtf_replace(link.href, 'imgur.com', 'i.imgur.com');
+        if (link.href.indexOf('www.') != - 1) {
+          link.href = wtf_replace(link.href, 'www.', '');
+        }
+      } 
+      else if (link.href.indexOf(',') != - 1)
+      {
+        link.title = 'Original at: ' + link.href;
+        link.href = link.href.split(',') .length;
+        link.href += '.png';
+        link.href = wtf_replace(link.href, 'imgur.com', 'i.imgur.com');
+        if (link.href.indexOf('www.') != - 1) {
+          link.href = wtf_replace(link.href, 'www.', '');
+        }
+      }
+    }
+  }
 }
-
 function wtf_replace(str, text, rwith)
 {
-	return str.substr(0, str.indexOf(text)) + rwith + str.substr(str.indexOf(text) + text.length);
+  return str.substr(0, str.indexOf(text)) + rwith + str.substr(str.indexOf(text) + text.length);
 }
